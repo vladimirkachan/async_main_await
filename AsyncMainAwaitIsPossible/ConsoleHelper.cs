841f9160836fe1c23b0bff55c55e1a70544a7d2b
vladimirkachan <vladimirkachan0@gmail.com>
2021-07-07 23:08:09 +0300

    Add project files.

---
+++ AsyncMainAwaitIsPossible/ConsoleHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncMainAwaitIsPossible
+{
+    public static class ConsoleHelper
+    {
+        const string lockObject = "console";
+        public static void PrintLine(this string text, ConsoleColor color = ConsoleColor.Gray)
+        {
+            lock (lockObject)
+            {
+                Console.ForegroundColor = color;
+                Console.WriteLine(text);
+                Console.ResetColor();
+            }
+        }
+        public static void PrintLine(this string text, int color)
+        {
+            PrintLine(text, (ConsoleColor)color);
+        }
+        public static void Print(this string text, ConsoleColor color = ConsoleColor.Gray)
+        {
+            lock (lockObject)
+            {
+                Console.ForegroundColor = color;
+                Console.Write(text);
+                Console.ResetColor();
+            }
+        }
+        public static void Print(this string text, int color)
+        {
+            Print(text, (ConsoleColor)color);
+        }
+    }
+}
---
+++ AsyncMainAwaitIsPossible/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncMainAwaitIsPossible
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            "This is async method Main with await in scope".PrintLine();
+            for (int i = 0; i < 3; ++i) await Task.Run(Demo);
+            List<Task> tasks = new();
+            for (int i = 0; i < 7; ++i) 
+                tasks.Add(Task.Factory.StartNew(Demo));
+            Task.WaitAll(tasks.ToArray());
+            "\nThe end of method Main".PrintLine();
+        }
+
+        static void Demo()
+        {
+            int threadId = Thread.CurrentThread.ManagedThreadId;
+            int color = threadId % 15 + 1;
+            $"Start Demo threadId {threadId}".PrintLine(color);
+            for (int i = 0; i < 30; ++i)
+            {
+                Thread.Sleep(100);
+                ". ".Print(color);
+            }
+            $"\nEnd Demo threadId {threadId}".PrintLine(color);
+        }
+    }
+}
